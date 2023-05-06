f9727ff0fb7a073dafa3ed1dc360976b888b100a
azamali123456 <aazam7246@gmail.com>
2023-05-06 20:40:42 +0500

    Update App

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: "https://service-provide-and-consultant-app.vercel.app/",
       },
     ],
     paths: {
