0bab9da00ed42dcc3f1318ca6f8fab0827fee752
azamali123456 <aazam7246@gmail.com>
2023-07-06 16:45:19 +0500

    Update New

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: "https://cooperative-foal-sundress.cyclic.app/",
       },
     ],
     components: {
